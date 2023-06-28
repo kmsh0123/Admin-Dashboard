752c6d1dcbb097cdbef6861af97e375d7c6a2f47
Phonemin11111 <phonemin01@gmail.com>
2023-06-29 01:14:16 +0630

    navbar and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { useState } from 'react'
 import User from './Components/User'
 import Home from './Components/Home'
+import Navbar from './Components/Navbar'
 
 function App() {
   
 
   return (
     <div className="font-poppins">
+      <Navbar/>
       <User/>
+      
     </div>
 
-    <>
-      <Home/>
-    </>
     
 
   )
--- src/Routes/Paths.jsx
+++ src/Routes/Paths.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import Home from '../Components/Home'
+import Navbar from '../Components/Navbar'
+
 
 const Paths = () => {
   return (
     <Routes>
+      <Navbar/>
         <Route path='/' element={<Home/>}/>
     </Routes>
   )
